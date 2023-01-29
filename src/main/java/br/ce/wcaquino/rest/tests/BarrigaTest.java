28881d6a76415ff910ad888b221857b032fe79c0
henriquemj <henriquemadureira05@gmail.com>
2023-01-29 17:24:42 -0300

    deve Incluir Conta Com Sucesso - retorno 201

--- src/main/java/br/ce/wcaquino/rest/tests/BarrigaTest.java
+++ src/main/java/br/ce/wcaquino/rest/tests/BarrigaTest.java
@@ -6,6 +6,9 @@ import br.ce.wcaquino.rest.core.BaseTest;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BarrigaTest extends BaseTest {
 	
 	@Test
@@ -16,5 +19,32 @@ public class BarrigaTest extends BaseTest {
 		.then()
 			.statusCode(401)
 		;
+	}
+	
+	// Email: teste2023@teste.com.br
+	// senha: 123456
+	
+	@Test
+	public void deveIncluirContaComSucesso() {
+		Map<String, String> login = new HashMap<>();
+		login.put("email", "teste2023@teste.com.br");
+		login.put("senha", "123456");
+		
+		String token = given()
+				.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token");
+		
+		given()
+			.header("Authorization", "JWT " + token)
+			.body("{ \"nome\": \"conta qualquer\"}")
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201)
+	;
 	}
 }

8edf71ae4b06921072f2e12eb482f2d81a74ff04
henriquemj <henriquemadureira05@gmail.com>
2023-02-03 18:46:38 -0300

    Teste de Contas - Refac

--- src/main/java/br/ce/wcaquino/rest/core/BaseTest.java
+++ src/main/java/br/ce/wcaquino/rest/core/BaseTest.java
@@ -11,7 +11,7 @@ public class BaseTest implements Constantes {
 	
 	@BeforeClass
 	public static void setup( ) {
-		System.out.println("Passou aqui");
+		System.out.println("Before base");
 		RestAssured.baseURI = APP_BASE_URL;
 		RestAssured.port = APP_PORT;
 		RestAssured.basePath = APP_BASE_PATH;
--- src/main/java/br/ce/wcaquino/rest/tests/refac/ContasTest.java
+++ src/main/java/br/ce/wcaquino/rest/tests/refac/ContasTest.java
@@ -1,18 +1,22 @@
 package br.ce.wcaquino.rest.tests.refac;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
 
+import br.ce.wcaquino.rest.core.BaseTest;
 import io.restassured.RestAssured;
 
-public class ContasTest {
+public class ContasTest extends BaseTest {
 	
 	@BeforeClass
 	public static void login() {
+		System.out.println("Before Conta");
 		Map<String, String> login = new HashMap<>();
 		login.put("email", "teste2023@teste.com.br");
 		login.put("senha", "123456");
@@ -30,5 +34,50 @@ public class ContasTest {
 		RestAssured.get("/reset").then().statusCode(200);
 		
 	}
-
+	
+	@Test
+	public void deveIncluirContaComSucesso() {
+		System.out.println("incluir");
+		given()
+			.body("{ \"nome\": \"Conta inserida\"}")
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201)
+			;
+	}
+	
+	@Test
+	public void AlterarContaComSucesso() {
+		System.out.println("alterar");
+		Integer CONTA_ID = getIdContaPeloNome("Conta para alterar");
+		
+		given()
+			.body("{ \"nome\": \"Conta alterada\" }")
+			.pathParam("id", CONTA_ID)
+		.when()
+			.put("/contas/{id}")
+		.then()
+		.log().all()
+			.statusCode(200)
+			.body("nome", is("Conta alterada"))
+	;
+  }
+	
+	@Test
+	public void naoDeveInserirContaMesmoNome() {
+		given()
+			.body("{ \"nome\": \"Conta mesmo nome\" }")
+		.when()
+			.post("/contas")
+		.then()
+		.log().all()
+			.statusCode(400)
+			.body("error", is("Já existe uma conta com esse nome!"))
+	;
+  }
+	
+	public Integer getIdContaPeloNome(String nome) {
+		return RestAssured.get("/contas?nome="+nome).then().extract().path("id[0]");
+	}
 }
